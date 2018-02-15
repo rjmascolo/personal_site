df40088bee87af751bdc101786c9af2ec4648a92
github username <github email address>
2018-02-15 16:32:38 -0500

    done

--- my-app/src/pages/Projects.js
+++ my-app/src/pages/Projects.js
@@ -8,8 +8,45 @@ class Projects extends React.Component {
     return (
       <section class="projects-section" id="projects">
           <h1>Projects</h1>
-          <img></img>
-          <p>Full stack web developer based out of NYC. Previously, I was working for the nonprofit, truth, in Washington DC, where I was introduced to the world of web development while project managing the recreation of my company’s website. Seeking to learn more, I enrolled in Flatiron School where I learned Javascript and Ruby and libraries like Ruby on Rails and React. I am quick learner, and continuously exploring new technologies to expand my knowledge.</p>
+          <div>
+            <h2>Marketr MGMT</h2>
+            <p>Marketing specific project management app for creative deliverables.</p>
+            <div>
+              <span class="languages-used-item">React</span>
+              <span class="languages-used-item">Redux</span>
+              <span class="languages-used-item">Rails</span>
+              <span class="languages-used-item">HTML</span>
+              <span class="languages-used-item">CSS</span>
+              <span class="languages-used-item">Postgres</span>
+              <span class="languages-used-item">Semantic UI</span>
+              <span class="languages-used-item">AWS</span>
+              <span class="languages-used-item">Draft.js</span>
+            </div>
+          </div>
+          <div>
+            <h2>Commun_ity</h2>
+            <p>An event based app that delegates tasks to users for upcoming events.</p>
+            <div>
+              <span class="languages-used-item">React</span>
+              <span class="languages-used-item">Rails API</span>
+              <span class="languages-used-item">Semantic UI</span>
+              <span class="languages-used-item">Postgres</span>
+              <span class="languages-used-item">HTML</span>
+              <span class="languages-used-item">CSS</span>
+            </div>
+          </div>
+          <div>
+            <h2>Really Good Photos</h2>
+            <p>A  photo gathering app that uses the user’s coordinates to show them photos in their area.</p>
+            <div class="languages-used">
+              <span>React</span>
+              <span>Redux</span>
+              <span>Rails</span>
+              <span>Semantic UI</span>
+              <span>AWS</span>
+              <span>Draft.js</span>
+            </div>
+          </div>
       </section>
     )
   }
