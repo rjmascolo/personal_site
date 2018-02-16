1f9d0cc95d11b7c2ffc5a4ba03dc14db33e17e2f
github username <rjmascolo@gmail.com>
2018-02-16 18:32:54 -0500

    added links to homepage buttons

--- my-app/src/NavBar.js
+++ my-app/src/NavBar.js
@@ -11,7 +11,7 @@ class NavBar extends React.Component {
   	         <li class="nav-li"><a href="#about-section"><i class="material-icons icon" >person</i><span id="nav-text">About Me</span></a></li>
              <li class="nav-li"><a href="#skills"><i class="material-icons icon" >code</i><span id="nav-text">Skills</span></a></li>
   	         <li class="nav-li"><a href="#projects"><i class="material-icons icon" >laptop</i><span id="nav-text">Projects</span></a></li>
-  	         <li class="nav-li"><a href="#experience"><i class="material-icons icon" >work</i><span id="nav-text">Experience</span></a></li>
+  	         {/* <li class="nav-li"><a href="#experience"><i class="material-icons icon" >work</i><span id="nav-text">Experience</span></a></li> */}
           </ul>
     )
   }
--- my-app/src/pages/Homepage.js
+++ my-app/src/pages/Homepage.js
@@ -13,10 +13,10 @@ class Homepage extends React.Component {
           <p class="home homepage-p">Full stack web developer based out of Brooklyn</p>
           <p className="home homepage-icon-container" >
 
-            <span><i className="fab fa-linkedin" id="icon-homepage"></i></span>
-            <span id="icon-homepage"><i className="far fa-file-alt"></i></span>
-            <span><i className="fab fa-github" id="icon-homepage"></i></span>
-            <span><i class="fab fa-medium" id="icon-homepage"></i></span>
+            <a href="https://www.linkedin.com/in/ryan-mascolo-10479962/" target="_blank"><span><i className="fab fa-linkedin" id="icon-homepage"></i></span></a>
+            {/* <span id="icon-homepage"><i className="far fa-file-alt"></i></span> */}
+            <a href="https://github.com/rjmascolo" target="_blank"><span><i className="fab fa-github" id="icon-homepage"></i></span></a>
+            <a href="https://medium.com/@rjmascolo" target="_blank"><span><i class="fab fa-medium" id="icon-homepage"></i></span></a>
           </p>
       </section>
     )
