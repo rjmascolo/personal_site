e6b6d6383eb04d0110fbfdb814e3543add66341c
github username <rjmascolo@gmail.com>
2018-02-19 18:29:25 -0500

    updated navbar to show the active sectionand added hover effects

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,11 +8,36 @@ import Projects from './pages/Projects'
 import Experience from './pages/Experience'
 
 class App extends Component {
+
+  state = {
+    currentPosition:'home'
+  }
+
+  componentDidMount() {
+      window.addEventListener('scroll', this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (event) => {
+    let scroll = document.documentElement.scrollTop
+    if (scroll >= 0 && scroll < 857) {
+      this.setState({currentPosition:'home'})
+    }else if(scroll > 857 && scroll < 1276){
+      this.setState({currentPosition:'about-me'})
+    }else if(scroll >= 1276 && scroll < 1697){
+      this.setState({currentPosition:'skills'})
+    } else if(scroll >= 1697){
+      this.setState({currentPosition:'projects'})
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <NavBar />
-        <Homepage />
+        <NavBar currentPosition={this.state.currentPosition} />
+        <Homepage  />
         <AboutMe />
         <Skills />
         <Projects />
