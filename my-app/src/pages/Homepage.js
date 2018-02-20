a45040fe681a8bdcf6008bc938d0ee20d62aaac4
github username <rjmascolo@gmail.com>
2018-02-20 14:36:30 -0500

    fixed homepage image

--- my-app/src/pages/Homepage.js
+++ my-app/src/pages/Homepage.js
@@ -9,15 +9,26 @@ class Homepage extends React.Component {
     return (
       <section class="main-section" id="homepage">
         <img src={image} width="100%" />
+        <div class="desktop-homepage-info">
           <h1 class="home homepage-h1" >Ryan Mascolo</h1>
           <p class="home homepage-p">Full stack web developer based out of Brooklyn</p>
           <p className="home homepage-icon-container" >
-
             <a href="https://www.linkedin.com/in/ryan-mascolo-10479962/" target="_blank"><span><i className="fab fa-linkedin" id="icon-homepage"></i></span></a>
-            {/* <span id="icon-homepage"><i className="far fa-file-alt"></i></span> */}
+              {/* <span id="icon-homepage"><i className="far fa-file-alt"></i></span> */}
             <a href="https://github.com/rjmascolo" target="_blank"><span><i className="fab fa-github" id="icon-homepage"></i></span></a>
             <a href="https://medium.com/@rjmascolo" target="_blank"><span><i class="fab fa-medium" id="icon-homepage"></i></span></a>
           </p>
+        </div>
+        <div class="mobile-homepage-info">
+          <h1 class="mobile homepage-h1" >Ryan Mascolo</h1>
+          <p class="mobile homepage-p">Full stack web developer based out of Brooklyn</p>
+          <p className="mobile homepage-icon-container" >
+            <a href="https://www.linkedin.com/in/ryan-mascolo-10479962/" target="_blank"><span><i className="fab fa-linkedin" id="icon-homepage"></i></span></a>
+              {/* <span id="icon-homepage"><i className="far fa-file-alt"></i></span> */}
+            <a href="https://github.com/rjmascolo" target="_blank"><span><i className="fab fa-github" id="icon-homepage"></i></span></a>
+            <a href="https://medium.com/@rjmascolo" target="_blank"><span><i class="fab fa-medium" id="icon-homepage"></i></span></a>
+          </p>
+        </div>
       </section>
     )
   }
